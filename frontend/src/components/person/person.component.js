310c395e1334e8ec7a72955ba1dd8ad3c3679eb1
alextheprophecy <91223807+alextheprophecy@users.noreply.github.com>
2024-01-08 09:56:33 +0100

    feat(click, hover): implemented hover and clickable people. Also implemented brightness and scale effects for visual appeal on click/hover

--- frontend/src/components/person/person.component.js
+++ frontend/src/components/person/person.component.js
@@ -2,31 +2,54 @@ import "../../styles/person/person.css";
 import {useState} from "react";
 
 const Person = (props) => {
-    const [colour, setColour] = useState("transparent")
+    const HOVERED_BRIGHT_INCR = 1.25
+    const UNFOCUSED_BRIGHT = 1
+    const HOVERED_SIZE_INCR = 1.05
+    const FOCUSED_SIZE_INCR = 1.15
+    const UNFOCUSED_SIZE = 1
+
+    const [size, setSize] = useState(1)
+    const [brightness, setBrightness] = useState(1)
+
     const [focus, setFocus] = useState(false)
 
     const onHover = () => {
-        setColour("red")
+        setBrightness(HOVERED_BRIGHT_INCR)
+        setSize(HOVERED_SIZE_INCR)
+
+    }
+    const exitHover = () => {
+        props.setFocusedPerson(-1)
+        setFocus(false)
+
+        setBrightness(UNFOCUSED_BRIGHT)
+        setSize(UNFOCUSED_SIZE)
+    }
+    const onClick = () => {
+        if(focus) showSubject()
+        props.setFocusedPerson(props.index)
+        setFocus(true)
+
+        setSize(FOCUSED_SIZE_INCR)
     }
 
-    const exitHover = () => {
-        setColour("transparent")
+    const showSubject = () => {
+
     }
 
     return (
         <div className={"person"}
              style={{
-                 transform: `rotateX(10deg) translate3d(${props.pos.x}px, ${props.pos.y}px, ${props.pos.z}px)`,
-                 filter: `contrast(${props.opacity}%)`,
-                 backgroundColor: `${colour}`
-
+                 transform: `rotateX(10deg) translate3d(${props.pos.x}px, ${props.pos.y}px, ${props.pos.z}px) scale(${size})`,
+                 filter: `contrast(${props.opacity}%) brightness(${brightness})`,
              }}
         >
             <img className={"profile"} src={`faces/${props.image}.png`}/>
+
             <svg viewBox={"0 0 500 500"}>
-                <g className={"hover-shape"} onMouseOver={onHover} onMouseOut={exitHover}>
-                    <ellipse fill="white" cx={"250"} cy={"175"} rx={"110"} ry={"125"}/>
-                    <path fill="white" d={"M 0 500 L 500 500 L 475 390 L 350 300 L 250 275 L 150 300 L 25 390 L 0 500 Z"}/>
+                <g className={"hover-shape"} onMouseOver={onHover} onMouseOut={exitHover} onClick={onClick}>
+                    <ellipse fill="transparent" cx={"250"} cy={"175"} rx={"110"} ry={"125"}/>
+                    <path fill="transparent" d={"M 0 500 L 500 500 L 475 390 L 350 300 L 250 275 L 150 300 L 25 390 L 0 500 Z"}/>
                 </g>
             </svg>
 
--- frontend/src/components/person/personqueue.component.js
+++ frontend/src/components/person/personqueue.component.js
@@ -6,25 +6,35 @@ const START_Z_DISTANCE = 150
 const ITEM_Z_DISTANCE = 55
 const ITEM_Y_DISTANCE = 45
 const ITEM_X_OFFSET = 35
+
+const UNFOCUSED_Z_OFFSET = 50;
+const UNFOCUSED_Y_OFFSET = 60;
+
 const PersonQueue = ({list, title}) => {
     const [queue, setQueue] = useState(list)
-    const [scrollPointer, setScrollPointer] = useState(0)
+    const [scrollIndex, setScrollIndex] = useState(0)
+    const [focused, setFocused] = useState(-1) //default -1: no person is focused
 
     const scrollHandle = (event) => {
         const delta = Math.max(-1, Math.min(1, (event.deltaY)))//.nativeEvent.wheelDelta || -event.nativeEvent.detail)))
-        setScrollPointer(Math.min(queue.length-1, Math.max(0, scrollPointer+delta)))
+        setScrollIndex(Math.min(queue.length - 1, Math.max(0, scrollIndex + delta)))
     }
 
     const people = () => {
         return queue.map((p, i) => {
-            const n = i-scrollPointer
-            if (n<0) return "" //if scroll past this index, dont show this person
-
-            return (<Person opacity={100 - 4 * n}
+            const n = i - scrollIndex
+            if (n < 0) return "" //if scroll past this index, dont show this person
+            let yOffset = 0
+            let zOffset = 0
+            if (focused !== -1 && focused !== i) {
+                yOffset = (i < focused ? UNFOCUSED_Y_OFFSET : -UNFOCUSED_Y_OFFSET)
+                zOffset = (i < focused ? UNFOCUSED_Z_OFFSET : -UNFOCUSED_Z_OFFSET)
+            }
+            return (<Person index={i} setFocusedPerson={setFocused} opacity={100 - 4 * n}
                             pos={{
                                 'x': Math.pow(-1, i) * ITEM_X_OFFSET,
-                                'y': -n * ITEM_Y_DISTANCE,
-                                'z': START_Z_DISTANCE - smoothingFunction(n) * ITEM_Z_DISTANCE
+                                'y': -n * ITEM_Y_DISTANCE + yOffset,
+                                'z': START_Z_DISTANCE - smoothingFunction(n) * ITEM_Z_DISTANCE + zOffset
                             }}
                             image={p.image}/>)
         })
